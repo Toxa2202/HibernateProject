e1d534367a1edfacead56c5538c1277b2ca8a93e
Toxa2202 <svyatoj@gmail.com>
2019-09-25 23:51:12 +0300

    springStartHW --> write same project to ElectricityConsumer objects. Done

--- springStartHW/src/main/java/com/saint/anthony/Main.java
+++ springStartHW/src/main/java/com/saint/anthony/Main.java
@@ -8,7 +8,8 @@ public class Main {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
                 "/META-INF/applicationContext.xml");
-
+        // We make two copies of switcher to show difference between prototype scope in xml and singleton
+        // UUID randomly generates String. When 'prototype' is OFF - two strings are the same, another - different
         Switcher switcher1 = context.getBean(Switcher.class);
         switcher1.turnOnDevices();
         System.out.println("Current device is: " + switcher1.getUuid());
